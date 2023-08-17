b0fd4bbacb78ac41e6bd4e6a72852fdd6a3837c4
Bruno-Vr <brunovictor.md@gmail.com>
2023-08-17 17:24:10 -0300

    feat: Estilização Css do Formulario

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Formulário de Cadastro de Produtos
+
+Este projeto consiste em um formulário para salvar uma lista de produtos em uma tabela.
+O objetivo deste projeto é praticar conceitos relacionados a lógica e na criação de um código semântico.
+
+[Visualizar projeto](https://formulario-kappa-ten.vercel.app/)
--- script.js
+++ script.js
@@ -4,21 +4,28 @@ function inserirDados() {
    var campoValor = document.getElementById('campoValor');
 
    if (campoTexto.value == '' | campoValor.value == '') {
-      alert("Nenhum dado inserido")
+      alert("Nome ou valor não inserido")
+
+   } else {
+
+      let table = document.getElementById('tableDados');
+      let tableHeight = document.getElementById('tableDados').clientHeight;
+      if (tableHeight < 380) {
+         const novaLinha = table.insertRow();
+
+         const celula1 = novaLinha.insertCell();
+         const celula2 = novaLinha.insertCell();
+
+         celula1.textContent = campoTexto.value;
+         celula2.textContent = campoValor.value;
+
+         campoTexto.value = '';
+         campoValor.value = '';
+      } else {
+         alert("Numero máximo de produtos Atingido")
+      }
    }
 
-   let tabela = document.getElementById('tableDados');
-
-   const novaLinha = tabela.insertRow();
-
-   const celula1 = novaLinha.insertCell();
-   const celula2 = novaLinha.insertCell();
-
-   celula1.textContent = campoTexto.value;
-   celula2.textContent = campoValor.value;
-
-   campoTexto.value = '';
-   campoValor.value = '';
 }
 
 
