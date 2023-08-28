2b434a63160f1a502e09890c0e073955cef4deed
Bruno-Vr <brunovictor.md@gmail.com>
2023-08-28 18:02:14 -0300

    feat: Refatorando CSS para Sass

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
+const campoTexto = document.getElementById('campoNome');
+const campoValor = document.getElementById('campoValor');
+const alertMessage = document.getElementById('alert-camp-name');
+const alertMessageSecondary = document.getElementById('alert-camp-value');
 
-function inserirDados() {
-   var campoTexto = document.getElementById('campoNome');
-   var campoValor = document.getElementById('campoValor');
+campoValor.addEventListener("input", function () {
+   valor = campoValor.value;
+   if (valor.length > 5) {
+      alert('O valor não pode ter mais de 5 dígitos.');
+      campoValor.value = '';
+   }
+})
 
-   if (campoTexto.value == '' | campoValor.value == '') {
-      alert("Nome ou valor não inserido")
+function verificaDados() {
 
+   if (campoTexto.value == '') {
+      campoTexto.classList.add('inputInvalid');
+      alertMessage.style.opacity = "1";
    } else {
-
-      let table = document.getElementById('tableDados');
-      let tableHeight = document.getElementById('tableDados').clientHeight;
-      if (tableHeight < 380) {
-         const novaLinha = table.insertRow();
-
-         const celula1 = novaLinha.insertCell();
-         const celula2 = novaLinha.insertCell();
-
-         celula1.textContent = campoTexto.value;
-         celula2.textContent = campoValor.value;
-
-         campoTexto.value = '';
-         campoValor.value = '';
-      } else {
-         alert("Numero máximo de produtos Atingido")
-      }
+      campoTexto.classList.remove('inputInvalid');
+      alertMessage.style.opacity = "0";
+   }
+   if (campoValor.value == '') {
+      campoValor.classList.add('inputInvalid');
+      alertMessageSecondary.style.opacity = "1";
+   } else {
+      campoValor.classList.remove('inputInvalid');
+      alertMessageSecondary.style.opacity = "0";
    }
 
+   if (campoTexto.value != '' & campoValor.value != '') {
+      inserirDados();
+      campoTexto.value = '';
+      campoValor.value = '';
+   }
+
+}
+
+function inserirDados() {
+   let table = document.getElementById('tableDados');
+   let tableHeight = document.getElementById('tableDados').clientHeight;
+   if (tableHeight < 380) {
+      const novaLinha = table.insertRow();
+
+      const celula1 = novaLinha.insertCell();
+      const celula2 = novaLinha.insertCell();
+
+      celula1.textContent = campoTexto.value;
+      celula2.textContent = campoValor.value;
+   } else {
+      alert("Número máximo de produtos Atingido")
+   }
 }
 
 
